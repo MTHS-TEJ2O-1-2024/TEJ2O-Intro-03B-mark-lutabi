3ce99059fe16bdbe6812db0b4145964bd0acb7e0
mark-lutabi <mark.lutabi@mths.ca>
2024-09-05 11:24:34 -0400

    changed comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: mark lutabi
+ * Created on: sept 5
+ * This program shows Hello,mark!
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, mark!')
